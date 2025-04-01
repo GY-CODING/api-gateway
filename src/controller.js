fb4702eba77f2d2d7f5088dfb2ab5d3236655bf5
ToxYc <ivi1000vm@gmail.com>
2025-04-01 19:05:13 +0200

    test 15

--- src/controller.js
+++ src/controller.js
@@ -8,6 +8,9 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.all("/health", (req, res) => {
     res.send("UP");
 });
