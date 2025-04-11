b3e4772df82c0e317ab357bd36e95e8cec319f27
ToxYc <ivi1000vm@gmail.com>
2025-04-11 15:24:51 +0200

    fix: too maney connections

--- src/apidocs-fetcher.js
+++ src/apidocs-fetcher.js
@@ -3,13 +3,21 @@ const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 
-module.exports = async function fetchAPIDocs(service) {    
+module.exports = async function fetchAPIDocs(service) {
     const client = new MongoClient("mongodb+srv://gycoding:iggycoding-05@fallofthegods.zsllqn9.mongodb.net/");
-    await client.connect();
     
-    const db = client.db("APIGateway");
-    const collection = db.collection("APIDocs");
-
-    const docs = await collection.find({ service }).toArray();
-    return docs;
+    try {
+        await client.connect();
+        
+        const db = client.db("APIGateway");
+        const collection = db.collection("APIDocs");
+    
+        const docs = await collection.find({ service }).toArray();
+        return docs;
+    } catch(error) {
+        console.error("Error fetching API docs: ", error);
+        throw error;
+    } finally {
+        await client.close();
+    }
 }
