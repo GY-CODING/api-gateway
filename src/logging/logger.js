e38a1b3333f9713aee96cf4015cb1d8ce43fd299
ToxYc <ivi1000vm@gmail.com>
2025-06-09 20:31:45 +0200

    feat: Update logs

--- src/logging/logger.js
+++ src/logging/logger.js
@@ -3,18 +3,22 @@ require('dotenv').config();
 
 module.exports = class Log {
     static info(data) {
+        console.info("[INFO] " + data);
         log(LogLevel.INFO, data);
     }
 
     static error(data) {
+        console.error("[ERROR] " + data);
         log(LogLevel.ERROR, data);
     }
 
     static warn(data) {
+        console.warn("[WARN] " + data);
         log(LogLevel.WARN, data);
     }
 
     static debug(data) {
+        console.debug("[DEBUG] " + data);
         log(LogLevel.DEBUG, data);
     }
 }
