bd4d525504b4bd674f62dfdb8a68572d91d2d049
ToxYc <ivi1000vm@gmail.com>
2025-06-09 19:45:03 +0200

    feat: Add Books API

--- src/controller.js
+++ src/controller.js
@@ -76,6 +76,27 @@ app.all(`${process.env.MESSAGES_PATH}/{*splat}`, async (req, res) => {
     res.status(response.status).send(response.body);
 });
 
+app.all(`${process.env.BOOKS_PATH}/{*splat}`, async (req, res) => {
+    let routes = [];
+
+    try {
+        routes = await fetchAPIDocs(process.env.BOOKS_PATH);
+    } catch (error) {
+        res.status(500).send(
+            new ApiException(
+                "GATEWAY_ERROR",
+                `An internal API Gateway error has occurred, sorry for the inconvenience.`,
+                500
+            ).toJSON()
+        );
+    }
+
+    const response = await routeRequest(req, routes);
+
+    res.set('Content-Type', response.contentType);
+    res.status(response.status).send(response.body);
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`API Gateway listening on port ${process.env.PORT}`)
 });
